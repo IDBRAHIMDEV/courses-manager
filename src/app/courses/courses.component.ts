33dbd756880684a09399f664234f3e77d36af166
Taha EL MALIANI <taha.elmaliani.justice@outlook.fr>
2019-04-28 13:21:23 +0200

    grid and list

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -8,17 +8,18 @@ import Swal from 'sweetalert2';
 })
 export class CoursesComponent implements OnInit {
 
+  list = true;
   image = "";
   onEdit = false;
   
   courses = [
-    {id: 1, label: "Angular", isFavoris: true, vote: {like: 5, disLike: 0}},
-    {id: 2, label: "VueJS", isFavoris: true, vote: {like: 1500, disLike: 80}},
-    {id: 3, label: "Java", isFavoris: false, vote: {like: 50, disLike: 10}},
-    {id: 4, label: "Php", isFavoris: true, vote: {like: 0, disLike: 0}}
+    {id: 1, label: "Angular", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, expedita.", isFavoris: true, vote: {like: 5, disLike: 0}, price: 85.5478, published: new Date()},
+    {id: 2, label: "VueJS", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, expedita.", isFavoris: true, vote: {like: 1500, disLike: 80}, price: 152.9978, published: new Date()},
+    {id: 3, label: "Java", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, expedita.", isFavoris: false, vote: {like: 50, disLike: 10}, price: 74.5668, published: new Date()},
+    {id: 4, label: "Php", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, expedita.", isFavoris: true, vote: {like: 0, disLike: 0}, price: 452.4321, published: new Date()}
   ];
 
-  course = {id: this.courses.length + 1, label: "", isFavoris: true, vote: {like: 0, disLike: 0}};
+  course = {id: this.courses.length + 1, label: "", isFavoris: true, vote: {like: 0, disLike: 0}, description: "", price: 0, published: new Date()};
 
   constructor() { }
 
@@ -27,7 +28,7 @@ export class CoursesComponent implements OnInit {
 
   addCourse() {
     this.courses.push(this.course);
-    this.course = {id: this.courses.length + 1, label: "", isFavoris: true, vote: {like: 0, disLike: 0}};
+    this.course = {id: this.courses.length + 1, label: "", isFavoris: true, vote: {like: 0, disLike: 0}, description: "", price: 0, published: new Date()};
   }
 
   editCourse(course) {
@@ -37,7 +38,7 @@ export class CoursesComponent implements OnInit {
 
   updateCourse() {
     this.onEdit = false;
-    this.course = {id: 0, label: "", isFavoris: true, vote: {like: 0, disLike: 0}};
+    this.course = {id: 0, label: "", isFavoris: true, vote: {like: 0, disLike: 0}, description: "", price: 0, published: new Date()};
   }
 
   deleteCourse(index) {
